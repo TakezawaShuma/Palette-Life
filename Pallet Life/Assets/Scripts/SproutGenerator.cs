051bb35d33a73f612dbd85c18e43569363918aab
HattoriMiki <dgs.hibiki7@gmail.com>
2018-05-18 13:51:47 +0900

    さっきインデックスに追加し忘れたやつ

---
+++ Pallet Life/Assets/Scripts/SproutGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SproutGenerator : MonoBehaviour {
+
+    public GameObject sproutPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+            GameObject sprout = Instantiate(sproutPrefab) as GameObject;
+            sprout.transform.position = new Vector3(1.6f, 0.7f, 10);
+    }
+}
