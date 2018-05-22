d79c33a46f554ef68c1d7eb1b649314a93d9153c
HattoriMiki <dgs.hibiki7@gmail.com>
2018-05-22 14:41:27 +0900

    自分でもわからない作業ツリーのやつ

---
+++ Pallet Life/Assets/Scripts/SproutController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SproutController : MonoBehaviour {
+
+    public GameObject sproutPrefab;
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = new Ray();
+            RaycastHit hit = new RaycastHit();
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+
+            //マウスクリックした場所からRayを飛ばし、オブジェクトがあればtrue 
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.gameObject.CompareTag("Sprout"))
+                {
+                    if(sproutPrefab != null)
+                    {
+                        Destroy(sproutPrefab);
+                    }
+                }
+            }
+        }
+    }
+}
